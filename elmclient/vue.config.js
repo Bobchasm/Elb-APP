824e70d811ed0ee3c17d7476b9ad0d73b0610743
村雨 <1548209395@qq.com>
2025-09-21 21:28:19 +0800

    接了商家我的、成功支付、注册的接口

--- elmclient/vue.config.js
+++ elmclient/vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   devServer: {
-  port: 8080
+    // 你可以保持前端在8081，或者让它不指定
+    // port: 8081, 
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 你的后端地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '' // 重写路径，将请求中的 '/api' 移除
+        }
+      }
+    }
   }
-  }
- 
+}
