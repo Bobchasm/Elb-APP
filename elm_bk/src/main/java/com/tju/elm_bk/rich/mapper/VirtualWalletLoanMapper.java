a3794e9d25fc4c27e21c0cd474eaee08992d3512
Bobchasm <3023244157@tju.edu.cn>
2025-11-13 15:39:39 +0800

    perf - 贷款列表按时间降序

--- elm_bk/src/main/java/com/tju/elm_bk/rich/mapper/VirtualWalletLoanMapper.java
+++ elm_bk/src/main/java/com/tju/elm_bk/rich/mapper/VirtualWalletLoanMapper.java
@@ -20,7 +20,7 @@ public interface VirtualWalletLoanMapper {
     @Update("update virtual_wallet_loan set repay_time = #{time} where id = #{id}")
     void repay(Long id, LocalDateTime time);
 
-    @Select("select * from virtual_wallet_loan where wallet_id = #{walletId} order by create_time")
+    @Select("select * from virtual_wallet_loan where wallet_id = #{walletId} order by create_time desc")
     List<VirtualWalletLoan> getLoanList(Long walletId);
 
     @Select("select * from virtual_wallet_loan where id = #{id}")
