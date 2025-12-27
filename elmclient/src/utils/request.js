fdae5421ed590915d72aaf518dacc17542f67a8f
GaoCan <3347127717@qq.com>
2025-12-27 10:11:22 +0800

    改回来

--- elmclient/src/utils/request.js
+++ elmclient/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. 创建 Axios 实例
 const request = axios.create({
-  baseURL: 'http://REDACTED_DOMAIN:8080', // 这里填你的后端基础路径（如 http://REDACTED_DOMAIN:8080），如果和前端同域可留空
+  baseURL: 'http://localhost:8080', // 这里填你的后端基础路径（如 http://localhost:8080），如果和前端同域可留空
   timeout: 15000, // 请求超时时间增加到15秒
   headers: {
     'Content-Type': 'application/json' // 默认请求格式
