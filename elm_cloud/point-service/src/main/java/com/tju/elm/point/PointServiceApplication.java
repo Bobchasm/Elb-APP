bd213afc4a557631297288e6e54206a1b15ba26c
Bobchasm <3023244157@tju.edu.cn>
2026-01-01 00:56:29 +0800

    perf - 积分系统扫描common配置

--- elm_cloud/point-service/src/main/java/com/tju/elm/point/PointServiceApplication.java
+++ elm_cloud/point-service/src/main/java/com/tju/elm/point/PointServiceApplication.java
@@ -7,12 +7,16 @@ import org.springframework.context.annotation.Import;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.tju.elm.point",
+                "config"
+        }
+)
 @MapperScan(
         basePackages = {"com.tju.elm.point.mapper"},
         annotationClass = org.apache.ibatis.annotations.Mapper.class
 )
-@Import(config.RedisConfig.class)
 public class PointServiceApplication {
 
     public static void main(String[] args) {
