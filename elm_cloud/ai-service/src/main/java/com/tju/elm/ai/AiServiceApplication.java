2cc1daf73caeea857527c15f219306b067f8ac46
Bobchasm <3023244157@tju.edu.cn>
2026-01-03 03:35:24 +0800

    fix - 修复服务配置问题

--- elm_cloud/ai-service/src/main/java/com/tju/elm/ai/AiServiceApplication.java
+++ elm_cloud/ai-service/src/main/java/com/tju/elm/ai/AiServiceApplication.java
@@ -3,14 +3,24 @@ package com.tju.elm.ai;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.tju.elm.ai",
+                "filters",
+                "com.tju.elm.api.config"
+        }
+)
 @MapperScan(
         basePackages = {"com.tju.elm.ai.mapper"},
         annotationClass = org.apache.ibatis.annotations.Mapper.class
 )
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.tju.elm.api.client")
 public class AiServiceApplication {
 
     public static void main(String[] args) {
