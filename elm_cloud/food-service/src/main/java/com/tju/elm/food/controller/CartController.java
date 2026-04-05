52af7d97507636c88365e4d673f1f8f5cd74dac3
Bobchasm <3023244157@tju.edu.cn>
2026-04-05 14:26:30 +0800

    fix - 暂时移除cart缓存,出了一些迷惑且不稳定的bug

--- elm_cloud/food-service/src/main/java/com/tju/elm/food/controller/CartController.java
+++ elm_cloud/food-service/src/main/java/com/tju/elm/food/controller/CartController.java
@@ -1,18 +1,17 @@
 package com.tju.elm.food.controller;
 
-import cn.hutool.core.thread.ThreadUtil;
 import com.tju.elm.food.annotation.CartCacheEvict;
-import com.tju.elm.food.annotation.CartCacheable;
 import com.tju.elm.food.aspect.CartCacheHelper;
-import com.tju.elm.food.mapper.FoodMapper;
 import com.tju.elm.food.pojo.vo.CartItemVO;
 import com.tju.elm.food.service.CartService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import result.HttpResult;
-import utils.UserContext;
 
 import java.util.List;
 
@@ -27,21 +26,21 @@ public class CartController {
 
     @GetMapping("/list")
     @Operation(summary = "获取用户在指定商家的购物车商品列表")
-    @CartCacheable(businessIdSpEL = "#businessId")
+//    @CartCacheable(businessIdSpEL = "#businessId")
     public HttpResult<List<CartItemVO>> listCartItem(@RequestParam Long businessId) {
         return HttpResult.success(cartService.getCartItemList(businessId));
     }
 
     @GetMapping("/add")
     @Operation(summary = "向购物车添加商品")
-    @CartCacheEvict(businessIdSpEL = "@cartCacheHelper.getBusinessIdByFoodId(#foodId)")
+//    @CartCacheEvict(businessIdSpEL = "@cartCacheHelper.getBusinessIdByFoodId(#foodId)")
     public HttpResult<Long> addCartItem(@RequestParam Long foodId, @RequestParam Integer quantity) {
 //        ThreadUtil.sleep(1000);
         return HttpResult.success(cartService.addItem(foodId, quantity));
     }
 
     @GetMapping("/quantity")
-    @Operation(summary = "修改购物车指定商品数量")
+//    @Operation(summary = "修改购物车指定商品数量")
     @CartCacheEvict(businessIdSpEL = "@cartCacheHelper.getBusinessIdByCartId(#cartId)")
     public HttpResult<Long> updateItemQuantity(@RequestParam Long cartId, @RequestParam Integer quantity) {
         return HttpResult.success(cartService.updateItem(cartId, quantity));
@@ -49,14 +48,14 @@ public class CartController {
 
     @GetMapping("/clear")
     @Operation(summary = "清空用户在指定商家的购物车")
-    @CartCacheEvict(businessIdSpEL = "#businessId")
+//    @CartCacheEvict(businessIdSpEL = "#businessId")
     public HttpResult<Long> clearCart(@RequestParam Long businessId) {
         return HttpResult.success(cartService.clearCart(businessId));
     }
 
     @GetMapping("/remove")
     @Operation(summary = "移除指定购物车商品")
-    @CartCacheEvict(businessIdSpEL = "@cartCacheHelper.getBusinessIdByCartId(#cartId)")
+//    @CartCacheEvict(businessIdSpEL = "@cartCacheHelper.getBusinessIdByCartId(#cartId)")
     public HttpResult<Long> removeItem(@RequestParam Long cartId) {
         return HttpResult.success(cartService.removeItem(cartId));
     }
