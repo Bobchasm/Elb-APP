3ac6c46283ae8e4ba9a5ecdc26744847f462a6c5
jia-siyun <2549407920@qq.com>
2026-04-21 22:22:07 +0800

    feat: add frontend production obfuscation

--- elmclient/vue.config.js
+++ elmclient/vue.config.js
@@ -1,5 +1,32 @@
+const WebpackObfuscator = require('webpack-obfuscator');
+
 module.exports = {
   devServer: {
     port: 80
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.devtool = false;
+      config.plugins = config.plugins || [];
+      config.plugins.push(
+        new WebpackObfuscator(
+          {
+            rotateStringArray: true,
+            stringArray: true,
+            stringArrayEncoding: ['base64'],
+            stringArrayThreshold: 0.75,
+            splitStrings: true,
+            splitStringsChunkLength: 8,
+            selfDefending: true,
+            simplify: true,
+            compact: true,
+            controlFlowFlattening: false,
+            deadCodeInjection: false,
+            disableConsoleOutput: false
+          },
+          ['chunk-vendors*.js']
+        )
+      );
+    }
   }
-}
+};
